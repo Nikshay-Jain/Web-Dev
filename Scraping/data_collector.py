cf04f1072dc19d8412c4516c2df064677d4cf4a3
Nikshay-Jain <nikshay.p.jain@gmail.com>
2025-02-09 00:23:12 +0530

    Amazon scrapped into csv

---
+++ Scraping/data_collector.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+
+def get_data(html):
+    try:
+        soup = BeautifulSoup(html, 'html.parser')
+        t = soup.find('h2')
+        try:
+            name = t.get_text().split('\n')[0]
+        except:
+            return [None, None, None, None]
+        
+        price = soup.find('span', class_='a-price-whole').text
+        rating = soup.find('span', attrs={'class': 'a-icon-alt'}).text
+        url = f"https://amazon.in/{soup.find('a', class_='a-link-normal s-line-clamp-2 s-link-style a-text-normal')['href']}"
+
+        return [name, price, rating, url]
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return [None, None, None, None]
+
+if __name__ == '__main__':
+    data = {"Name": [], "Price": [], "Rating": [], "URL": []}
+    for file in os.listdir('data'):
+        d = get_data(open(f"data/{file}", 'r', encoding='utf-8').read())
+        data['Name'].append(d[0])
+        data['Price'].append(d[1])
+        data['Rating'].append(d[2])
+        data['URL'].append(d[3])
+
+        print(f"Data from data/{file} added to data.")
+    pd.DataFrame(data).to_csv('data.csv', index=False)
