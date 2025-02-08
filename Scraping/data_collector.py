4aeb69e5e51c9210ac8ff0d81ac430fa8ca64444
Nikshay-Jain <nikshay.p.jain@gmail.com>
2025-02-09 00:29:01 +0530

    Basic Formatting

--- Scraping/data_collector.py
+++ Scraping/data_collector.py
@@ -23,12 +23,13 @@ def get_data(html):
 
 if __name__ == '__main__':
     data = {"Name": [], "Price": [], "Rating": [], "URL": []}
+
     for file in os.listdir('data'):
         d = get_data(open(f"data/{file}", 'r', encoding='utf-8').read())
         data['Name'].append(d[0])
         data['Price'].append(d[1])
         data['Rating'].append(d[2])
         data['URL'].append(d[3])
-
         print(f"Data from data/{file} added to data.")
+        
     pd.DataFrame(data).to_csv('data.csv', index=False)
